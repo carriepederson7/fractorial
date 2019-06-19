d75570a731fac8935fb0d6c0923107212f074acc
jg <stratacast@comcast.net>
2019-06-18 19:03:11 -0700

    solved equation to calculate factorials

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,15 @@ $(document).ready(function() {
   $("#fractorial").submit(function(event) {
     event.preventDefault();
 
-    // var inputNumber = parseInt($("input#number".val()));
     var inputNumber = parseInt($("input#number").val());
-
-    var total = 0
-    for (var currentNumber = inputNumber; currentNumber <= inputNumber; currentNumber += 1)
-    // {
-    //   total += currentNumber;
-    // }
+    var total = 1;
+    for (var currentNumber = 1; currentNumber <= inputNumber; currentNumber += 1) {
+      total *= currentNumber;
+    }
 
     var result = total;
-    console.log(total)
-    $("#result").show();
 
-  });
-});
+    $("#answer").text(result);
+
+  })
+})
